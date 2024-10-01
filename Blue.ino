688c5fa1a1a2dd858801f368b4fca00821d4bfc7
Abdiraxman Bade <badea@mcmaster.ca>
2024-10-01 16:35:19 -0400

    Update Blue.ino

--- Blue.ino
+++ Blue.ino
@@ -1,26 +1,17 @@
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
 #include "BluetoothSerial.h"
 
 BluetoothSerial SerialBT;
 
-// Define GPIO pins
+// Define GPIO pins for LEDs
+const int ledPin1 = 5;  // GPIO 5 for LED 1
+const int ledPin2 = 7;  // GPIO 4 for LED 2
+const int ledPin3 = 8; // GPIO 13 for LED 3
+const int analogPin = 34; // GPIO 34 for analog input (thermistor/potentiometer)
 
-const int ledPin1 = 5;  
-const int ledPin2 = 7; 
-const int ledPin3 = 8; 
-const int analogPin = 34; /
-// BLE Characteristics
-BLECharacteristic *pCharacteristic;
+// Variable for simulated temperature
 float temperature = 0.0;
 String ledStatus = "";
 String condition = "";
-
-// BLE UUIDs
-#define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID    "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 // Function to simulate temperature from analog input
 float readTemperature() {
@@ -29,50 +20,38 @@ float readTemperature() {
   return temp;
 }
 
-
-
 // Function to update LED status based on temperature
-void updateLEDs(float temp) 
-{
-  
+void updateLEDs(float temp) {
   if (temp < 33) {
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, LOW);
-    ledStatus = "LED1 OFF, LED2 OFF, LED3 OFF";
-    condition = "It's kinda cold in here.";
-    
-  } else if (temp >= 33 && temp < 50) {
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, HIGH);
-    ledStatus = "LED1 OFF, LED2 OFF, LED3 ON";
-    condition = "Nice and cozy";
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, LOW);
+      ledStatus = "LED1 OFF, LED2 OFF, LED3 OFF";
+      condition = "It's kinda cold in here.";
+      
+    } else if (temp >= 33 && temp < 50) {
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, LOW);
+      digitalWrite(ledPin3, HIGH);
+      ledStatus = "LED1 OFF, LED2 OFF, LED3 ON";
+      condition = "Nice and cozy";
 
-  } else if (temp >= 50 && temp < 66) {
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    ledStatus = "LED1 OFF, LED2 ON, LED3 ON";
-    condition = "Getting a bit hot.";
-    
-  } else {
-    digitalWrite(ledPin1, HIGH);
-    digitalWrite(ledPin2, HIGH);
-    digitalWrite(ledPin3, HIGH);
-    ledStatus = "LED1 ON, LED2 ON, LED3 ON";
-    condition = "I'M BURNING";
-    //printf(temp);
-  }
+    } else if (temp >= 50 && temp < 66) {
+      digitalWrite(ledPin1, LOW);
+      digitalWrite(ledPin2, HIGH);
+      digitalWrite(ledPin3, HIGH);
+      ledStatus = "LED1 OFF, LED2 ON, LED3 ON";
+      condition = "Getting a bit hot.";
+      
+    } else {
+      digitalWrite(ledPin1, HIGH);
+      digitalWrite(ledPin2, HIGH);
+      digitalWrite(ledPin3, HIGH);
+      ledStatus = "LED1 ON, LED2 ON, LED3 ON";
+      condition = "I'M BURNING";
+      //printf(temp);
+    }
 }
-
-// BLE callback
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-  }
-  void onDisconnect(BLEServer* pServer) {
-  }
-};
 
 void setup() {
   // Initialize Serial Monitor
@@ -83,38 +62,27 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
 
-  // Initialize BLE
-  BLEDevice::init("ESP32 Temperature Monitor");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ |
-                      BLECharacteristic::PROPERTY_WRITE |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
-  pCharacteristic->addDescriptor(new BLE2902());
-
-  pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->start();
+  // Start Classic Bluetooth
+  SerialBT.begin("ESP32"); 
+  Serial.println("The device started, now you can pair it with Bluetooth!");
 }
 
 void loop() {
-  // Read and calculate temperature
+  // Read temperature
   temperature = readTemperature();
+
+  // Update LEDs based on temperature
   updateLEDs(temperature);
 
-  // Send data over BLE
+  // Prepare data string
   String data = "Temperature: " + String(temperature, 1) + "C, " + ledStatus + "              " + condition;
-  pCharacteristic->setValue(data.c_str());
-  pCharacteristic->notify();
 
+  // Send data over Bluetooth
+  SerialBT.println(data); 
 
-  SerialBT.flush();
+  // Debugging: print data to Serial Monitor
   Serial.println(data);
-  delay(1000);
+
+  delay(100); 
 }
+
